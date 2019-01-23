41cf6e27e2ee950eea99793b839a84de4297e727
Georgiy-Vwfe <postnikov.george@yandex.ru>
2019-01-23 14:41:34 +0300

    create contacts and greeting html pages

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,6 +1,7 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-    private static final String[] contact = {"Hi, my name is %s!", "My number is %s"};
+    private static final String contact = "My number is %s";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
     @RequestMapping("/contacts")
-    public Contacts[] contacts(@RequestParam(value="name", defaultValue="<Content not paste>") String name) {
-        return new Contacts[]{
-                new Contacts(counter.incrementAndGet(), String.format(contact[0], name)),
-                new Contacts(counter.incrementAndGet(), String.format(contact[1], name))
-        };
+    public Contacts contacts(@RequestParam(value = "name", defaultValue = "<Content not paste>") String name) {
+        return new Contacts(counter.incrementAndGet(), String.format(contact, name));
     }
 }
